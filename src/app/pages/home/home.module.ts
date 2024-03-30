0c41a6da1e74bf5aa44b8553efde776e797b4f56
dmaksiuta <shutle369@gmail.com>
2024-03-30 12:48:37 +0200

    added products, added product id page, added back arrow

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'product/:id', component: ProductComponent },
 ];
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ProductComponent
   ],
   imports: [
     CommonModule,
